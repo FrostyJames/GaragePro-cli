91d99feba671440fbdae51830e738884e8d795ff
james <ivanlavan773@gmail.com>
2025-08-27 20:50:07 +0300

    added custom interactive menu

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from cli import cli
+import click
+from cli import customer_cli, vehicle_cli, service_cli
 
-if __name__ == '__main__':
-    cli()
+@click.group()
+def main():
+    """GaragePro CLI - Main Menu"""
+    pass
+
+main.add_command(customer_cli)
+main.add_command(vehicle_cli)
+main.add_command(service_cli)
+
+if __name__ == "__main__":
+    main()
